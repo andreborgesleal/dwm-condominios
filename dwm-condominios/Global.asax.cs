3db3db9315a9faec3e126d0f803d9579d1479883
unknown <afbleal@gmail.com>
2016-06-20 23:22:10 -0300

    Global.asax.cs (controle de sessão) e _Menu.cshtml (exibir o menu pelo nome curto)

--- dwm-condominios/Global.asax.cs
+++ dwm-condominios/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using App_Dominio.Entidades;
+using App_Dominio.Security;
+using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +16,25 @@ namespace dwm_condominios
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void Session_Start(object sender, EventArgs e)
+        {
+            HttpContext.Current.Session.Add("__MyAppSession", string.Empty);
+        }
+
+        public void Session_End(object sender, EventArgs e)
+        {
+            EmpresaSecurity<App_DominioContext> login = new EmpresaSecurity<App_DominioContext>();
+            if (System.Web.HttpContext.Current != null)
+                login.EncerrarSessao(System.Web.HttpContext.Current.Session.SessionID);
+        }
+
+        protected void Application_End()
+        {
+            EmpresaSecurity<App_DominioContext> login = new EmpresaSecurity<App_DominioContext>();
+            if (System.Web.HttpContext.Current != null)
+                login.EncerrarSessao(System.Web.HttpContext.Current.Session.SessionID);
         }
     }
 }
